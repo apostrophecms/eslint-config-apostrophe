62c22b798b59ee0f3bcc10dd35ec3d2e91a59ecd
Alex Bea <abea@mailbox.org>
2020-03-03 11:12:09 -0600

    Applies curly new-line rules to objects as well

--- index.js
+++ index.js
@@ -3,6 +3,39 @@ module.exports = {
   "rules": {
     "brace-style": ["warn", "1tbs"],
     "curly": ["warn", "all"],
+    'object-curly-newline': ['warn', {
+      // Object expressions can have one property on a single line.
+      // e.g., `const foo = { bar: 1 };`.
+      'ObjectExpression': {
+        'minProperties': 2,
+        'consistent': true,
+        'multiline': true
+      },
+      // Object destructuring can have two properties on a single line.
+      // e.g., `const { foo, bar }= baz;`.
+      'ObjectPattern': {
+        'minProperties': 3,
+        'consistent': true,
+        'multiline': true
+      },
+      // Import declarations can have two properties on a single line.
+      // e.g., `import { foo, bar } from 'baz';`.
+      'ImportDeclaration': {
+        'minProperties': 3,
+        'consistent': true,
+        'multiline': true
+      },
+      // Export declarations can have two properties on a single line.
+      // e.g., `export { foo, bar };`.
+      'ExportDeclaration': {
+        'multiline': true,
+        'consistent': true,
+        'minProperties': 3
+      }
+    }],
+    'object-property-newline': [
+      'warn', { 'allowAllPropertiesOnSameLine': false }
+    ],
     'semi': [ 'error', 'always' ],
     'no-unused-vars': ['error', { 'varsIgnorePattern': 'apos', 'args': 'none', 'ignoreRestSiblings': true }],
     "space-before-function-paren": 0,
