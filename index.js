a37eba9f4f33b4844a54456320aaef95815cef66
Alex Bea <abea@mailbox.org>
2020-06-17 09:32:00 -0500

    Updates version and changelog

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
   'extends': 'standard',
-  "rules": {
-    "brace-style": ["warn", "1tbs"],
-    "curly": ["warn", "all"],
+  'rules': {
+    'brace-style': ['warn', '1tbs'],
+    'curly': ['warn', 'all'],
     'object-curly-newline': ['warn', {
       // Object expressions can have one property on a single line.
       // e.g., `const foo = { bar: 1 };`.
@@ -38,55 +38,55 @@ module.exports = {
     ],
     'semi': [ 'error', 'always' ],
     'no-unused-vars': ['error', { 'varsIgnorePattern': 'apos', 'args': 'none', 'ignoreRestSiblings': true }],
-    "space-before-function-paren": 0,
-    "padded-blocks": 0,
-    "quotes": ['warning', 'single'],
-    "standard/no-callback-literal": 0,
-    "no-path-concat": 0,
-    "no-throw-literal": 0,
-    "no-console": 1,
-    "import/no-extraneous-dependencies": 1
+    'space-before-function-paren': 0,
+    'padded-blocks': 0,
+    'quotes': ['warning', 'single'],
+    'standard/no-callback-literal': 0,
+    'no-path-concat': 0,
+    'no-throw-literal': 0,
+    'no-console': 1,
+    'import/no-extraneous-dependencies': 1
   },
-  "overrides": [
+  'overrides': [
     {
-      "files": [ "**/public/**/*.js" ],
-      "excludedFiles": [ "**/public/vendor/**/*.js" ],
-      "globals": {
-        "window": true,
-        "document": true,
-        "location": true,
-        "apos": true,
-        "_": true,
-        "async": true,
-        "confirm": true,
-        "$": true,
-        "CKEDITOR_BASEPATH": true,
-        "CKEDITOR": true,
-        "alert": true,
-        "jQuery": true,
-        "sluggo": true,
-        "moog": true,
-        "Pikaday": true,
-        "moment": true
+      'files': [ '**/public/**/*.js' ],
+      'excludedFiles': [ '**/public/vendor/**/*.js' ],
+      'globals': {
+        'window': true,
+        'document': true,
+        'location': true,
+        'apos': true,
+        '_': true,
+        'async': true,
+        'confirm': true,
+        '$': true,
+        'CKEDITOR_BASEPATH': true,
+        'CKEDITOR': true,
+        'alert': true,
+        'jQuery': true,
+        'sluggo': true,
+        'moog': true,
+        'Pikaday': true,
+        'moment': true
       }
     },
     {
-      "files": [ "test/**/*.js" ],
-      "globals": {
-        "describe": true,
-        "it": true,
-        "after": true,
-        "before": true
+      'files': [ 'test/**/*.js' ],
+      'globals': {
+        'describe': true,
+        'it': true,
+        'after': true,
+        'before': true
       },
-      "rules": {
-        "no-console": 0
+      'rules': {
+        'no-console': 0
       }
     },
     {
-      "files": ["*.js"],
-      "excludedFiles": ["**/public/**/*.js"],
-      "rules": {
-        "no-var": 0
+      'files': ['*.js'],
+      'excludedFiles': ['**/public/**/*.js'],
+      'rules': {
+        'no-var': 0
       }
     }
   ]
